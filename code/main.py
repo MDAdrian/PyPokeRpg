d9ec13479afb98f941fad622084d4692948c453f
MDAdrian <adi.maniu33@gmail.com>
2025-04-21 22:01:58 +0300

    Import World

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 from settings import *
+from pytmx.util_pygame import load_pygame
+from os.path import join
 
 
 class Game:
@@ -6,7 +8,17 @@ class Game:
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("PyPokeRpg")
+
+        self.import_assets()
+        self.setup(self.tmx_maps['world'], 'house')
     
+    def import_assets(self):
+        self.tmx_maps = {'world': load_pygame(join('data', 'maps', 'world.tmx'))}
+
+    def setup(self, tmx_map, player_start_pos):
+        for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
+            print(x * TILE_SIZE, y * TILE_SIZE, surf)
+
     def run(self):
         while True:
             # event loop
