39fe4f395253fec5320140798595168f363e1128
MDAdrian <adi.maniu33@gmail.com>
2025-04-23 20:37:15 +0300

    Initialized Overworld

--- code/main.py
+++ code/main.py
@@ -2,12 +2,17 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
 
+from sprites import Sprite
+
 
 class Game:
     def __init__(self):
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("PyPokeRpg")
+
+        # groups
+        self.all_sprites = pygame.sprite.Group()
 
         self.import_assets()
         self.setup(self.tmx_maps['world'], 'house')
@@ -17,7 +22,7 @@ class Game:
 
     def setup(self, tmx_map, player_start_pos):
         for x,y,surf in tmx_map.get_layer_by_name('Terrain').tiles():
-            print(x * TILE_SIZE, y * TILE_SIZE, surf)
+            Sprite((x * TILE_SIZE, y * TILE_SIZE), surf, self.all_sprites)
 
     def run(self):
         while True:
@@ -28,6 +33,7 @@ class Game:
                     exit()
 
             # game logic
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
 
 if __name__ == '__main__':
