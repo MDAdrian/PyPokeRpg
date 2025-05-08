4e3c3ee75c6a64eda9b640f8357415ae76d77a5f
MDAdrian <adi.maniu33@gmail.com>
2025-05-08 21:16:41 +0300

    Added basic water

--- code/main.py
+++ code/main.py
@@ -2,13 +2,14 @@ from settings import *
 from pytmx.util_pygame import load_pygame
 from os.path import join
 
-from sprites import Sprite
+from sprites import AnimatedSprite, Sprite
 from entities import Player
 from groups import AllSprites
 
+from support import *
 
 class Game:
-    def __init__(self):
+    def __init__(self): 
         pygame.init()
         self.display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
         pygame.display.set_caption("PyPokeRpg")
@@ -18,13 +19,17 @@ class Game:
         self.all_sprites = AllSprites()
 
         self.import_assets()
-        self.setup(self.tmx_maps['hospital'], 'world')
+        self.setup(self.tmx_maps['world'], 'house')
     
     def import_assets(self):
         self.tmx_maps = {
             'world': load_pygame(join('data', 'maps', 'world.tmx')),
             'hospital': load_pygame(join('data', 'maps', 'hospital.tmx'))
         }
+        self.overworld_frames = {
+            'water': import_folder('graphics', 'tilesets', 'water')
+        }
+        
 
     def setup(self, tmx_map, player_start_pos):
         # terrain
@@ -41,6 +46,11 @@ class Game:
             if obj.name == 'Player' and obj.properties['pos'] == player_start_pos:
                 self.player = Player((obj.x, obj.y), self.all_sprites)
 
+        # water
+        for obj in tmx_map.get_layer_by_name('Water'):
+            for x in range(int(obj.x), int(obj.x + obj.height), TILE_SIZE):
+                for y in range(int(obj.y), int(obj.y + obj.height), TILE_SIZE):
+                    AnimatedSprite((x,y), self.overworld_frames['water'], self.all_sprites)
 
     def run(self):
         while True:
