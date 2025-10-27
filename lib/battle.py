6e164e752d336ea7872ddf5cc11b070bba482323
MDAdrian <adi.maniu33@gmail.com>
2025-10-27 21:43:35 +0200

    Display player monster init

--- lib/battle.py
+++ lib/battle.py
@@ -1,8 +1,12 @@
 import pygame
 
+from lib.settings import BATTLE_POSITIONS
+from lib.sprites import MonsterSprite
+
 
 class Battle:
     def __init__(self, player_monsters, opponent_monsters, monster_frames, bg_surface, fonts):
+        # general
         self.display_surface = pygame.display.get_surface()
         self.bg_surface = bg_surface
         self.monster_frames = monster_frames
@@ -12,5 +16,27 @@ class Battle:
             'opponent_monsters': opponent_monsters,
         }
 
+        # groups
+        self.battle_sprites = pygame.sprite.Group()
+        self.player_sprites = pygame.sprite.Group()
+        self.opponent_sprites = pygame.sprite.Group()
+        self.setup()
+
+    def setup(self):
+        for entity, monster in self.monster_data.items():
+            for index, monster in {k:v for k, v in monster.items() if k <= 2}.items():
+                self.create_monster(monster, index, index, entity)
+
+    def create_monster(self, monster, index, pos_index, entity):
+        frames = self.monster_frames['monsters'][monster.name]
+        if entity == 'player':
+            pos = list(BATTLE_POSITIONS['left'].values())[pos_index]
+            groups = (self.battle_sprites, self.player_sprites)
+        # else:
+        #     pos =
+        #     groups =
+            MonsterSprite(pos, frames, groups, monster, index, pos_index, entity)
+
     def update(self, dt):
         self.display_surface.blit(self.bg_surface, (0,0))
+        self.battle_sprites.draw(self.display_surface)
