4ac40cd90bf9433a492db49ea8eb592e5c29798a
MDAdrian <adi.maniu33@gmail.com>
2025-10-28 20:46:53 +0200

    Monster Level and Level Up

--- lib/sprites.py
+++ lib/sprites.py
@@ -3,6 +3,8 @@ from random import uniform
 import pygame
 
 from lib.settings import *
+from lib.support import draw_bar
+
 
 # overworld sprites
 class Sprite(pygame.sprite.Sprite):
@@ -91,6 +93,12 @@ class MonsterLevelSprite(pygame.sprite.Sprite):
 		self.font = font
 		self.image = pygame.Surface((60, 26))
 		self.rect = self.image.get_frect(topleft = pos) if entity == 'player' else self.image.get_frect(topright = pos)
+		self.xp_rect = pygame.FRect(0, self.rect.height - 2, self.rect.width, 2)
 
 	def update(self, _):
 		self.image.fill(COLORS['white'])
+		text_surf = self.font.render(f'Lvl: {self.monster_sprite.monster.level}', False, COLORS['black'])
+		text_rect = text_surf.get_frect(center = (self.rect.width / 2, self.rect.height / 2))
+		self.image.blit(text_surf, text_rect)
+
+		draw_bar(self.image, self.xp_rect, self.monster_sprite.monster.xp, self.monster_sprite.monster.level_up, COLORS['black'], COLORS['white'], radius=0)
